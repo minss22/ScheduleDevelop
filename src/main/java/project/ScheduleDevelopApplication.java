54642e7db2a999bf1c66e87593c97a09cde657f1
minss22 <kms141516@gmail.com>
2025-11-11 15:48:14 +0900

    ✨ feat: Lv 1. 일정 Entity 클래스 생성
    - Schedule 클래스
      - 일정 정보를 저장하는 Entity 클래스
      - 일정 제목, 일정 내용, 유저명 저장
      - 고유 식별자(ID)는 자동으로 생성하여 저장
      - 작성일, 수정일 필드는 ScheduleBaseEntity를 상속받아 자동으로 저장
    - ScheduleBaseEntity 클래스
      - JPA Auditing 사용하여 엔티티의 생성 및 수정 시간을 자동으로 관리
    - main 클래스
      - @EnableJpaAuditing 추가하여 Auditing 인프라 활성화

--- src/main/java/project/ScheduleDevelopApplication.java
+++ src/main/java/project/ScheduleDevelopApplication.java
@@ -2,7 +2,13 @@ package project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * Main 클래스
+ * - 프로그램 시작점
+ */
+@EnableJpaAuditing // Auditing 인프라 활성화
 @SpringBootApplication
 public class ScheduleDevelopApplication {
 
---
+++ src/main/java/project/schedule/entity/Schedule.java
@@ -0,0 +1,51 @@
+package project.schedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.schedule.dto.CreateScheduleRequest;
+import project.schedule.dto.UpdateScheduleRequest;
+
+/**
+ * Schedule 클래스
+ * - 일정 정보를 저장하는 Entity 클래스
+ * - 일정 제목, 일정 내용, 유저명 저장
+ * - 고유 식별자(ID)는 자동으로 생성하여 저장
+ * - 작성일, 수정일 필드는 BaseEntity를 상속받아 자동으로 저장
+ */
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends ScheduleBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 일정 고유 ID
+    @Column(length = 50, nullable = false)
+    private String name; // 작성 유저명
+    @Column(length = 50, nullable = false)
+    private String title; // 일정 제목
+    @Column(nullable = false)
+    private String content; // 일정 내용
+
+    /**
+     * 유저명, 일정 제목, 일정 내용 저장 (Create)
+     * @param request 일정 생성 Request DTO
+     */
+    public Schedule(CreateScheduleRequest request) {
+        this.name = request.getName();
+        this.title = request.getTitle();
+        this.content = request.getContent();
+    }
+
+    /**
+     * 유저명, 일정 제목, 일정 내용 수정 (Update)
+     * @param request 일정 수정 Request DTO
+     */
+    public void update(UpdateScheduleRequest request) {
+        this.name = request.getName();
+        this.title = request.getTitle();
+        this.content = request.getContent();
+    }
+}
