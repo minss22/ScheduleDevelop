3c258b0645e266e170dab1b5357514b2f6bfd6b3
minss22 <kms141516@gmail.com>
2025-11-19 16:32:57 +0900

    💄 style: 일정 응답 정보 수정
    - `ScheduleResponse` 클래스: 일정 작성자 정보 추가

--- src/main/java/project/schedule/dto/ScheduleResponse.java
+++ src/main/java/project/schedule/dto/ScheduleResponse.java
@@ -13,17 +13,21 @@ public class ScheduleResponse {
     private final Long id;
     private final String title;
     private final String content;
+    private final Long userId; // 일정 작성자 고유 ID
+    private final String userName; // 일정 작성자 이름
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 
     /**
-     * Schedule Entity를 이용한 생성자
+     * Schedule Entity를 이용하여 정보를 저장하는 생성자
      * @param schedule 일정 Entity
      */
     public ScheduleResponse(Schedule schedule) {
         this.id = schedule.getId();
         this.title = schedule.getTitle();
         this.content = schedule.getContent();
+        this.userId = schedule.getUser().getId();
+        this.userName = schedule.getUser().getName();
         this.createdAt = schedule.getCreatedAt();
         this.updatedAt = schedule.getModifiedAt();
     }
